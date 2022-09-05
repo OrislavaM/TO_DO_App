35d43538a96963574dfae3ea1358aa2a3bb9454b
Oryslava <orisyamatvienko@gmail.com>
2022-09-05 14:32:19 +0200

    add clear create-react-app

---
+++ todo-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Login from './Pages/Login';
+import Registration from './Pages/Registration';
+
+function App() {
+  return (
+    <div className="App">
+      <Registration/>
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
---
+++ todo-app/src/Pages/Todo_list.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Todo_list(){
+    return(
+        <div>Todo_list</div>
+    )
+}
+
+export default Todo_list
